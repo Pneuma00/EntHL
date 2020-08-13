b93d495fa9ea6357c031859954bab2595d95d48f
Pneuma <syca0714@gmail.com>
2020-08-13 10:15:57 +0900

    .

---
+++ script.js
@@ -0,0 +1,21 @@
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.href = '//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/styles/atom-one-dark.min.css';
+document.head.appendChild(style);
+
+var font = document.createElement('style');
+font.innerHTML = `
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap');
+pre { font-family: 'Ubuntu Mono', monospace; } span { font-family: inherit }`;
+document.body.appendChild(font);
+
+var hl = document.createElement('script');
+hl.src = '//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/highlight.min.js';
+document.head.appendChild(hl);
+
+var content = document.querySelector('.discussContentPlain > div');
+content.innerHTML = content.innerHTML
+    .replace(/&lt;code(\/[a-zA-Z]+)?&gt;\n/g, match => `<pre class="${match.slice(8, -5) ? match.slice(9, -5) : 'plaintext'}"><code>`)
+    .replace(/&lt;\/code&gt;\n/g, '</code></pre>');
+
+hl.addEventListener('load', () => { hljs.initHighlighting() });
